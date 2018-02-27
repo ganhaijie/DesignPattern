6a467970f29032d2b3dc48511f805702e3bb5071
ganhaijie <ganhaijie1028@gmail.com>
2018-02-27 13:22:26 +0800

    饿汉

--- src/com/ghj/example/singleton/A.java
+++ src/com/ghj/example/singleton/A.java
@@ -1,8 +1,20 @@
 package com.ghj.example.singleton;
 
+/**
+ * 饿汉模式
+ */
 public class A {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static A instance = new A();
+
+    private A(){
+
+    }
+
+    public static A getInstance(){
+        return instance;
     }
 }
+
+//Y 类加载的时候完成实例化 避免了线程的同步问题
+//N 无法达到懒加载的效果 没有用到这个实例  造成内存浪费 可忽略
